701cd997ec81f9e32b16846a3a150273d3d50025
MehdiBidjedCS <mehdi.zerbita@univ-constantine2.dz>
2025-01-05 22:38:34 +0100

    prepare workspace

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <div className='w-full h-full bg-red-400 flex  '>
+      hello world
+    </div>
+  )
+}
+
+export default App
